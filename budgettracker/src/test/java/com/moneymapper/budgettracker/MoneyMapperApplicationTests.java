6040db875f58b9e33c060756ca13aebe4717bb70
Juan <juan64@usf.edu>
2025-05-06 17:10:12 -0400

    Added spring initializer folder

---
+++ budgettracker/src/test/java/com/moneymapper/budgettracker/MoneyMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneymapper.budgettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
